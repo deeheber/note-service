016fc1b175bfe060c4193645c08eaf7c53c957c5
danielle <danielle@stackery.io>
2018-12-03 14:22:26 -0800

    Start create function

--- src/create/index.js
+++ src/create/index.js
@@ -1,5 +1,25 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB();
+
 exports.handler = async message => {
   console.log(message);
+  const data = JSON.parse(message.body);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      id: uuid.v1(),
+      content: data.content,
+      author: data.author,
+      createdAt: new Date().getTime()
+    }
+  };
+
+  console.log(`Adding user to table ${process.env.TABLE_NAME}`);
+  const result = await dynamodb.put(params).promise();
+  console.log(`User added to table, done`);
+  console.log(`RESULT: ${result}`);
 
   return {
     statusCode: 200,
