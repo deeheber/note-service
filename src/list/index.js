9f661aa2d96da8d02db10be35fe8cb74ea2b8123
danielle <danielle@stackery.io>
2018-12-03 15:40:54 -0800

    List function

--- src/list/index.js
+++ src/list/index.js
@@ -1,9 +1,20 @@
-exports.handler = async message => {
-  console.log(message);
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
+exports.handler = async event => {
+  console.log(event);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Select: 'ALL_ATTRIBUTES'
+  };
+
+  const result = await dynamodb.scan(params).promise();
+
+  // TODO add in error handling
   return {
     statusCode: 200,
     headers: {},
-    body: JSON.stringify({ it: 'works' })
+    body: JSON.stringify(result.Items)
   };
 };
