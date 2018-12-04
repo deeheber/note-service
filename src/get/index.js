75b314415b6730edab991b77b92b51e9a64999b9
danielle <danielle@stackery.io>
2018-12-03 16:52:39 -0800

    Get by id function content

--- src/get/index.js
+++ src/get/index.js
@@ -1,5 +1,20 @@
-exports.handler = async message => {
-  console.log(message);
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  return {};
-}
+exports.handler = async event => {
+  console.log(event);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: event.pathParameters.id
+  };
+
+  const result = await dynamodb.get(params).promise();
+
+  // TODO add in error handling
+  return {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify(result.Item)
+  };
+};
