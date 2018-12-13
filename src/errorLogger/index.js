f531e3b179432574b80ae32cc6fc85de3b6798ff
danielle <danielle@stackery.io>
2018-12-13 15:17:43 -0800

    Error handling finessing

--- src/errorLogger/index.js
+++ src/errorLogger/index.js
@@ -1,11 +1,11 @@
 exports.handler = async message => {
-  console.log(`ERROR: ${message}`);
+  console.log(`ERROR: ${JSON.stringify(message)}`);
 
   return {
     statusCode: 500,
     headers: {},
     body: JSON.stringify({
-      message: 'There was an internal server error'
+      message: 'There was an internal server error. Please check your logs to debug.'
     })
   };
 };
