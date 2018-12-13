8285cff8801077330e58a54fbedc6dcd43d9ac3f
danielle <danielle@stackery.io>
2018-12-13 14:54:15 -0800

    Delete function

--- src/remove/index.js
+++ src/remove/index.js
@@ -1,5 +1,23 @@
-exports.handler = async message => {
-  console.log(message);
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  return {};
-}
+exports.handler = async event => {
+  console.log(event);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: { id: event.pathParameters.id }
+  };
+
+  console.log(`Deleting note in table ${process.env.TABLE_NAME}`);
+  const result = await dynamodb.delete(params).promise();
+  console.log(`Note deleted in table, done`);
+  console.log('RESULT: ', result);
+
+  // TODO: add in error handling
+  return {
+    statusCode: 200,
+    headers: {},
+    body: `Note ${event.pathParameters.id} has been deleted.`
+  };
+};
