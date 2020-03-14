6e41c3ebe367791408467e9f7a7a132e230b0271
danielle <danielle@stackery.io>
2020-03-14 11:42:33 -0700

    Update remove function

--- src/remove/index.js
+++ src/remove/index.js
@@ -4,19 +4,35 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
+  const id = event.pathParameters.id;
 
   const params = {
     TableName: process.env.TABLE_NAME,
-    Key: { id: event.pathParameters.id }
+    Key: { id },
+    ExpressionAttributeValues: {
+      ':id': id
+    },
+    ConditionExpression: 'id = :id'
   };
 
-  console.log(`Deleting note in table ${process.env.TABLE_NAME}`);
-  const result = await dynamodb.delete(params).promise();
-  console.log(`Note deleted in table, done`);
+  let response;
+  let statusCode;
+  try {
+    await dynamodb.delete(params).promise();
+    response = { id };
+    statusCode = 200;
+  } catch (err) {
+    console.log(`ERROR: ${JSON.stringify(err.message, undefined, 2)}`);
+    response = err.message;
+    if (err.code === 'ConditionalCheckFailedException') {
+      response = 'Item not found, unable to delete';
+    }
+    statusCode = err.statusCode || 500;
+  }
 
   return {
-    statusCode: 200,
+    statusCode,
     headers: {},
-    body: JSON.stringify(result)
+    body: JSON.stringify(response)
   };
 };
