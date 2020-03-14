87c1b8c4da4fe9c2fa54ad3d8d3d08efea1a2630
danielle <danielle@stackery.io>
2020-03-14 11:25:58 -0700

    Update update function

--- src/update/index.js
+++ src/update/index.js
@@ -5,25 +5,40 @@ exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
   const data = JSON.parse(event.body);
+  const id = event.pathParameters.id;
 
   const params = {
     TableName: process.env.TABLE_NAME,
-    Key: { id: event.pathParameters.id },
+    Key: { id },
     UpdateExpression: 'SET content = :content, updatedAt = :updatedAt',
     ExpressionAttributeValues: {
       ':content': data.content ? data.content : null,
-      ':updatedAt': new Date().getTime()
+      ':updatedAt': new Date().getTime(),
+      ':id': id
     },
+    ConditionExpression: 'id = :id',
     ReturnValues: 'ALL_NEW'
   };
 
-  console.log(`Updating note in table ${process.env.TABLE_NAME}`);
-  const result = await dynamodb.update(params).promise();
-  console.log(`Note updated in table, done`);
+  let response;
+  let statusCode;
+  try {
+    const result = await dynamodb.update(params).promise();
+    console.log('result ', result);
+    response = result.Attributes;
+    statusCode = 200;
+  } catch (err) {
+    console.log(`ERROR: ${JSON.stringify(err.message, undefined, 2)}`);
+    response = err.message;
+    if (err.code === 'ConditionalCheckFailedException') {
+      response = 'Item not found, unable to update';
+    }
+    statusCode = err.statusCode || 500;
+  }
 
   return {
-    statusCode: 200,
+    statusCode,
     headers: {},
-    body: JSON.stringify(result.Attributes)
+    body: JSON.stringify(response)
   };
 };
